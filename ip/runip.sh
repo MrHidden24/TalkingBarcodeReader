162b17d804691038a74f779e24850c4e4b4e1fb7
MrHidden24 <89341592+MrHidden24@users.noreply.github.com>
2023-08-11 02:35:46 +0300

    Add files via upload

--- ip/runip.sh
+++ ip/runip.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+#sudo python3 /var/www/html/upc/bin/lcd/demo_lcd_process_bar.py
+
+#DIR="/var/www/html/ip"
+
+#cd $DIR
+sudo python /var/www/html/ip/demo_clock_and_IP.py
+#sudo nohup python3 $DIR/barcode.py &
 
